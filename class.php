563273fbed753f2e79995686a9a2c169242526ea
moorthy21sparkout <163838347+moorthy21sparkout@users.noreply.github.com>
2024-04-05 17:09:49 +0530

    class basic

---
+++ class.php
@@ -0,0 +1,37 @@
+<?php
+/*function add(){
+    $a=10;
+    $b=23;
+    $c=$a+$b;
+    return $c;
+}
+function sub()
+{
+    $a=10;
+    $b=1;
+    $c=$a*$b;
+    return $c;
+}
+echo "total value is: ".add();
+echo "<br> total value is:".sub();
+*/
+class opp{
+    public $a,$b,$c;//property.
+
+    function add()
+    {
+        $this->c=$this->a+$this->b;
+        return $this->c;
+    }
+    function sub()
+    {
+        $this->c=$this->a-$this->b;
+        return $this->c;
+    }
+}
+$obj=new opp();
+$obj->a=12;
+$obj->b=14;
+echo "toatal value is:".$obj->sub();
+echo "<br>toatal value is:".$obj->add();
+?>
